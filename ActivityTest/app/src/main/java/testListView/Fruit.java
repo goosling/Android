475fc69df6fb8fb38dc1908cde734aff4622be32
goosling <xiaoliu920@163.com>
2016-01-22 17:13:02 +0800

    listview

---
+++ ActivityTest/app/src/main/java/testListView/Fruit.java
@@ -0,0 +1,22 @@
+package testListView;
+
+/**
+ * Created by joe on 2016/1/21.
+ */
+public class Fruit {
+    private String name;
+    private int imageId;
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+}
