9b263ed02a901ed35f8b055bd453f0bde7603aed
goosling <xiaoliu920@163.com>
2016-02-28 08:55:14 +0800

    new test

---
+++ AllTest/app/src/main/java/com/example/joe/alltest/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.example.joe.alltest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by JOE on 2016/2/27.
+ */
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
