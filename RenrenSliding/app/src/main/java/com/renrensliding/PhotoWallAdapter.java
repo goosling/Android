75fd63ffc3c88b6884f68408b4e5f355c00a0e65
goosling <xiaoliu920@163.com>
2016-03-18 16:03:01 +0800

    photo wall

---
+++ RenrenSliding/app/src/main/java/com/renrensliding/PhotoWallAdapter.java
@@ -0,0 +1,222 @@
+package com.renrensliding;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.LruCache;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import db.Image;
+
+/**
+ * Created by JOE on 2016/3/18.
+ */
+public class PhotoWallAdapter extends ArrayAdapter<String> implements AbsListView.OnScrollListener {
+
+    //记录所有正在下载或等待下载的任务
+    private Set<BitmapWorkerTask> taskCollection;
+
+    private LruCache<String, Bitmap> mMemoryCache;
+
+    private GridView mPhotoWall;
+
+    //第一章可见图片的下标
+    private int mFirstVisibleItem;
+
+    //一屏有多少张图片可见
+    private int mItemVisibleCount;
+
+    private boolean isFirstEnter = true;
+
+    public PhotoWallAdapter(Context context, int textViewResId, String[] objects,
+                            GridView photoWall) {
+        super(context, textViewResId, objects);
+        mPhotoWall = photoWall;
+        taskCollection = new HashSet<BitmapWorkerTask>();
+        //获取应用程序最大可用内存
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory());
+        int cacheSize = maxMemory / 8;
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getByteCount();
+            }
+        };
+        mPhotoWall.setOnScrollListener(this);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final String url = getItem(position);
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.photo_layout, null);
+        } else {
+            view = convertView;
+        }
+
+        final ImageView photo = (ImageView) view.findViewById(R.id.photo);
+        photo.setTag(url);
+        setImageView(url, photo);
+        return view;
+    }
+
+    private void setImageView(String imageUrl, ImageView imageView) {
+        Bitmap bitmap = getBitmapFromMemoryCache(imageUrl);
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+        } else {
+            imageView.setImageResource(R.mipmap.ic_launcher);
+        }
+    }
+
+    /**
+     * 将一张图片存储到LruCache中。
+     *
+     * @param key    LruCache的键，这里传入图片的URL地址。
+     * @param bitmap LruCache的键，这里传入从网络上下载的Bitmap对象。
+     */
+    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemoryCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    public Bitmap getBitmapFromMemoryCache(String key) {
+        return mMemoryCache.get(key);
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        if (scrollState == SCROLL_STATE_IDLE) {
+            loadBitmaps(mFirstVisibleItem, mItemVisibleCount);
+        } else {
+            cacelAllTasks();
+        }
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        mFirstVisibleItem = firstVisibleItem;
+        mItemVisibleCount = visibleItemCount;
+        // 下载的任务应该由onScrollStateChanged里调用，但首次进入程序时onScrollStateChanged并不会调用，
+        // 因此在这里为首次进入程序开启下载任务。
+        if (isFirstEnter && visibleItemCount > 0) {
+            loadBitmaps(firstVisibleItem, visibleItemCount);
+            isFirstEnter = false;
+        }
+    }
+
+    /**
+     * 加载Bitmap对象。此方法会在LruCache中检查所有屏幕中可见的ImageView的Bitmap对象，
+     * 如果发现任何一个ImageView的Bitmap对象不在缓存中，就会开启异步线程去下载图片。
+     *
+     * @param firstVisibleItem 第一个可见的ImageView的下标
+     * @param visibleItemCount 屏幕中总共可见的元素数
+     */
+    private void loadBitmaps(int firstVisibleItem, int visibleItemCount) {
+        try {
+            for (int i = firstVisibleItem; i < firstVisibleItem + visibleItemCount; i++) {
+                String imageUrl = Image.imageThumbUrls[i];
+                Bitmap bitmap = getBitmapFromMemoryCache(imageUrl);
+                if (bitmap == null) {
+                    BitmapWorkerTask task = new BitmapWorkerTask();
+                    taskCollection.add(task);
+                    task.execute(imageUrl);
+                }else {
+                    ImageView imageView = (ImageView)mPhotoWall.findViewWithTag(imageUrl);
+                    if(imageView != null && bitmap != null) {
+                        imageView.setImageBitmap(bitmap);
+                    }
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 取消所有正在下载或等待下载的任务。
+     */
+    public void cacelAllTasks() {
+        if(taskCollection != null) {
+            for(BitmapWorkerTask task : taskCollection) {
+                task.cancel(false);
+            }
+        }
+    }
+
+
+    /**
+     * 异步下载图片的任务。
+     *
+     * @author guolin
+     */
+    class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
+
+        /**
+         * 图片的URL地址
+         */
+        private String imageUrl;
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            imageUrl = params[0];
+            // 在后台开始下载图片
+            Bitmap bitmap = downloadBitmap(params[0]);
+            if (bitmap != null) {
+                // 图片下载完成后缓存到LrcCache中
+                addBitmapToMemoryCache(params[0], bitmap);
+            }
+            return bitmap;
+        }
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            // 根据Tag找到相应的ImageView控件，将下载好的图片显示出来。
+            ImageView imageView = (ImageView) mPhotoWall.findViewWithTag(imageUrl);
+            if (imageView != null && bitmap != null) {
+                imageView.setImageBitmap(bitmap);
+            }
+            taskCollection.remove(this);
+        }
+
+        /**
+         * 建立HTTP请求，并获取Bitmap对象。
+         *
+         * @param imageUrl
+         *            图片的URL地址
+         * @return 解析后的Bitmap对象
+         */
+        private Bitmap downloadBitmap(String imageUrl) {
+            Bitmap bitmap = null;
+            HttpURLConnection con = null;
+            try {
+                URL url = new URL(imageUrl);
+                con = (HttpURLConnection) url.openConnection();
+                con.setConnectTimeout(5 * 1000);
+                con.setReadTimeout(10 * 1000);
+                bitmap = BitmapFactory.decodeStream(con.getInputStream());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (con != null) {
+                    con.disconnect();
+                }
+            }
+            return bitmap;
+        }
+    }
+}
