40c72a3427cc1ea6ee699fb57d897eaa275fa341
goosling <xiaoliu920@163.com>
2016-03-28 16:20:48 +0800

    view route

--- AnroidTrainingCourse/app/src/main/java/com/example/joehu/anroidtrainingcourse/MainActivity.java
+++ AnroidTrainingCourse/app/src/main/java/com/example/joehu/anroidtrainingcourse/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
