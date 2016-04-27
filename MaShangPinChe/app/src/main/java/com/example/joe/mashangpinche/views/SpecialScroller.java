4a41e55a4f6b5de681840bca00be7e3ff91ccca3
goosling <xiaoliu920@163.com>
2016-04-27 20:51:54 +0800

    mashangpinche

--- MaShangPinChe/app/src/main/java/com/example/joe/mashangpinche/views/SpecialScroller.java
+++ MaShangPinChe/app/src/main/java/com/example/joe/mashangpinche/views/SpecialScroller.java
@@ -3,5 +3,14 @@ package com.example.joe.mashangpinche.views;
 /**
  * Created by joe on 2016/4/26.
  */
+
+import android.annotation.TargetApi;
+import android.widget.OverScroller;
+
+@TargetApi(9)
 public class SpecialScroller {
+
+    private OverScroller mScroller = null;
+
+    
 }
