4dbe23d0c860ae81b35ad2caa7572101b9a4c9eb
goosling <xiaoliu920@163.com>
2016-06-09 21:11:21 +0800

    main

--- SimpleNews/app/src/main/java/com/example/joe/simplenews/main/widget/MainActivity.java
+++ SimpleNews/app/src/main/java/com/example/joe/simplenews/main/widget/MainActivity.java
@@ -1,7 +1,108 @@
 package com.example.joe.simplenews.main.widget;
 
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.joe.simplenews.R;
+import com.example.joe.simplenews.about.AboutFragment;
+import com.example.joe.simplenews.images.widget.ImageFragment;
+import com.example.joe.simplenews.main.presenter.MainPresenter;
+import com.example.joe.simplenews.main.presenter.MainPresenterImpl;
+import com.example.joe.simplenews.main.view.MainView;
+import com.example.joe.simplenews.news.widget.NewsFragment;
+
 /**
  * Created by JOE on 2016/6/8.
  */
-public class MainActivity {
+public class MainActivity extends AppCompatActivity implements MainView {
+
+    private DrawerLayout mDrawerLayout;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private Toolbar mToolbar;
+
+    private NavigationView mNavigationView;
+
+    private MainPresenter mMainPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.drawer_open, R.string.drawer_close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mNavigationView = (NavigationView)findViewById(R.id.navigation_view);
+        setupDrawerContent(mNavigationView);
+
+        mMainPresenter = new MainPresenterImpl(this);
+
+        switch2News();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == item.getItemId()) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView mNavigationView) {
+        mNavigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener(){
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        mMainPresenter.switchNavigation(item.getItemId());
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void switch2News() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new NewsFragment()).commit();
+        mToolbar.setTitle(R.string.navigation_news);
+    }
+
+    @Override
+    public void switch2Images() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new ImageFragment()).commit();
+        mToolbar.setTitle(R.string.navigation_images);
+    }
+
+    @Override
+    public void switch2Weather() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new WeatherFragment()).commit();
+        mToolbar.setTitle(R.string.navigation_weather);
+    }
+
+    @Override
+    public void switch2About() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new AboutFragment()).commit();
+        mToolbar.setTitle(R.string.navigation_about);
+    }
 }
