8d08121ef310607ec659482bb50afdec6b94843f
goosling <xiaoliu920@163.com>
2016-06-28 21:24:38 +0800

    adapter

---
+++ WeatherClock/app/src/main/java/com/example/joe/weatherclock/adapter/AlarmClockAdapter.java
@@ -0,0 +1,19 @@
+package com.example.joe.weatherclock.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.widget.AdapterView;
+
+/**
+ * Created by JOE on 2016/6/28.
+ */
+public class AlarmClockAdapter extends RecyclerView.Adapter<>{
+
+
+    /**
+     * 保存控件实例
+     */
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        MaterialRippleLayout
+    }
+
+}
