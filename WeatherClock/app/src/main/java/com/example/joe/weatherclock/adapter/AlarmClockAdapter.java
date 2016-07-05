6ae2ab9241b541a42ffccd3507229798852fb84a
goosling <xiaoliu920@163.com>
2016-07-05 14:25:42 +0800

    adapter

--- WeatherClock/app/src/main/java/com/example/joe/weatherclock/adapter/AlarmClockAdapter.java
+++ WeatherClock/app/src/main/java/com/example/joe/weatherclock/adapter/AlarmClockAdapter.java
@@ -1,7 +1,12 @@
 package com.example.joe.weatherclock.adapter;
 
 import android.support.v7.widget.RecyclerView;
-import android.widget.AdapterView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.example.joe.weatherclock.R;
 
 /**
  * Created by JOE on 2016/6/28.
@@ -13,7 +18,27 @@ public class AlarmClockAdapter extends RecyclerView.Adapter<AlarmClockAdapter.My
      * 保存控件实例
      */
     class MyViewHolder extends RecyclerView.ViewHolder {
-        public MyViewHolder() {}
+        MaterialRippleLayout rippleView;
+
+        TextView time;
+
+        TextView repeat;
+
+        TextView tag;
+
+        ToggleButton tglBtn;
+
+        ImageView deleteBtn;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            rippleView = (MaterialRippleLayout)itemView.findViewById(R.id.ripple_view);
+            time = (TextView)itemView.findViewById(R.id.tv_time);
+            repeat = (TextView)itemView.findViewById(R.id.tv_repeat);
+            tag = (TextView)itemView.findViewById(R.id.tv_tag);
+            tglBtn = (ToggleButton)
+        }
+
     }
 
 }
