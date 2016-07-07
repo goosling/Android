f55c080d7b54ad33fe6086857dbe7c74474a45b8
goosling <xiaoliu920@163.com>
2016-07-07 23:36:23 +0800

    adapter:

--- WeatherClock/app/src/main/java/com/example/joe/weatherclock/adapter/AlarmClockAdapter.java
+++ WeatherClock/app/src/main/java/com/example/joe/weatherclock/adapter/AlarmClockAdapter.java
@@ -1,5 +1,6 @@
 package com.example.joe.weatherclock.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -7,12 +8,49 @@ import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.example.joe.weatherclock.R;
+import com.example.joe.weatherclock.bean.AlarmClock;
+
+import java.util.List;
 
 /**
  * Created by JOE on 2016/6/28.
  */
 public class AlarmClockAdapter extends RecyclerView.Adapter<AlarmClockAdapter.MyViewHolder>{
 
+
+    private final Context mContext;
+
+    /**
+     * 是否显示删除按钮
+     */
+    private boolean mIsDisplayDeleteBtn = false;
+
+    /**
+     * 白色
+     */
+    private int mWhite;
+
+    /**
+     * 淡灰色
+     */
+    private int mWhiteTrans;
+
+    private List<AlarmClock> mList;
+
+    private boolean isCanClick = true;
+
+    public void setIsCanClick(boolean isCanClick) {
+        this.isCanClick = isCanClick;
+    }
+
+    public AlarmClockAdapter(Context context, List<AlarmClock> list) {
+        mContext = context;
+        mList = list;
+        mWhite = mContext.getResources().getColor(android.R.color.white);
+        mWhiteTrans = mContext.getResources().getColor(R.color.white_trans30);
+    }
+
+    
 
     /**
      * 保存控件实例
